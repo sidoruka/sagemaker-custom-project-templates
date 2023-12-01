1c8200a1348b5b92ede1f291aa598c6c4fab32a5
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2023-12-01 12:15:00 +0300

    Update iam-service-catalog-launch.tf

--- sm-project-tf-github/service-catalog-setup/iam-service-catalog-launch.tf
+++ sm-project-tf-github/service-catalog-setup/iam-service-catalog-launch.tf
@@ -18,6 +18,7 @@ resource "aws_iam_role" "sc_launch_iam_role" {
   name               = var.sc_product_launch_role
   path               = "/Launch/Constraint/"
   description        = "Role to be Assumed by Service Catalog Product Launch"
+  permissions_boundary = var.sc_common_roles_boundary
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
