a578c1097ef5cd7a259d47b383b8cb80b6d81cf8
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2023-12-02 23:24:35 +0300

    Update seed-code-checkin-codebuild.tf

--- sm-project-tf-github/sagemaker-project-setup/seed-code-checkin-codebuild.tf
+++ sm-project-tf-github/sagemaker-project-setup/seed-code-checkin-codebuild.tf
@@ -13,7 +13,7 @@ resource "aws_codebuild_project" "seed_code_checkin_build" {
 
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image        = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    image        = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
     type         = "LINUX_CONTAINER"
   }
 
