e01cb3944d6fd958a6d69d904c133403b5b642b6
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2023-12-03 00:03:10 +0300

    Update model-codebuild.tf

--- sm-project-tf-github/sagemaker-project-setup/model-codebuild.tf
+++ sm-project-tf-github/sagemaker-project-setup/model-codebuild.tf
@@ -13,7 +13,7 @@ resource "aws_codebuild_project" "model_pipeline_build" {
 
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image        = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    image        = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
     type         = "LINUX_CONTAINER"
 
     environment_variable {
